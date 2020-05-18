be5f3f370c3ad0757e74c47071955e3fa9540c02
artim-anton <1rimola0>
2020-05-18 16:29:41 +0300

    add Youtube activity

--- app/src/main/java/com/artimanton/workoutathome/MainActivity.kt
+++ app/src/main/java/com/artimanton/workoutathome/MainActivity.kt
@@ -2,6 +2,7 @@ package com.artimanton.workoutathome
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -49,5 +50,9 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun youtube(view: View) {
+
     }
 }
